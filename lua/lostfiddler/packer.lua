aa7cc51b95572f60921c6b1ed3d4a47526eac836
lostfiddler <h3lloiain@gmail.com>
2025-02-16 18:49:47 -0600

    enable auto tags for jsx

--- lua/lostfiddler/packer.lua
+++ lua/lostfiddler/packer.lua
@@ -57,7 +57,24 @@ return require('packer').startup(function(use)
         end
     }
 
-    use 'alvan/vim-closetag' -- autocomplete X(HTML) tags
+    use({
+        "alvan/vim-closetag",
+        config = function()
+            vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml"
+            vim.g.closetag_xhtml_filenames = "*.xhtml,*.jsx,*.tsx"
+            vim.g.closetag_filetypes = "html,xhtml,phtml"
+            vim.g.closetag_xhtml_filetypes = "xhtml,jsx"
+            vim.g.closetag_emptyTags_caseSensitive = 1
+            vim.g.closetag_regions = {
+                ["typescript.tsx"] = "jsxRegion,tsxRegion",
+                ["javascript.jsx"] = "jsxRegion",
+                ["typescriptreact"] = "jsxRegion,tsxRegion",
+                ["javascriptreact"] = "jsxRegion",
+            }
+            vim.g.closetag_shortcut = ">"
+            vim.g.closetag_close_shortcut = "<leader>>"
+        end,
+    })
     use({
         "kylechui/nvim-surround",
         config = function()
