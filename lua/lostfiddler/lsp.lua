ace9218263c582e4c90dd63a37c4f4d24961e957
lostfiddler <h3lloiain@gmail.com>
2025-04-06 12:01:09 -0500

    update

---
+++ lua/lostfiddler/lsp.lua
@@ -0,0 +1,6 @@
+vim.diagnostic.config({
+    virtual_text = false
+})
+
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
--- lua/lostfiddler/packer.lua
+++ lua/lostfiddler/packer.lua
@@ -57,22 +57,19 @@ return require('packer').startup(function(use)
         end
     }
 
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'nvim-tree/nvim-web-devicons', opt = true},
+        config = function()
+            require('lualine').setup({})
+        end
+    }
+    use "prettier/vim-prettier"
+
     use({
         "alvan/vim-closetag",
         config = function()
-            vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml"
-            vim.g.closetag_xhtml_filenames = "*.xhtml,*.jsx,*.tsx"
-            vim.g.closetag_filetypes = "html,xhtml,phtml"
-            vim.g.closetag_xhtml_filetypes = "xhtml,jsx"
-            vim.g.closetag_emptyTags_caseSensitive = 1
-            vim.g.closetag_regions = {
-                ["typescript.tsx"] = "jsxRegion,tsxRegion",
-                ["javascript.jsx"] = "jsxRegion",
-                ["typescriptreact"] = "jsxRegion,tsxRegion",
-                ["javascriptreact"] = "jsxRegion",
-            }
-            vim.g.closetag_shortcut = ">"
-            vim.g.closetag_close_shortcut = "<leader>>"
+            vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.js,*.ts,*.jsx,*.tsx"
         end,
     })
     use({
