31f121699f3af25088436021716e2da15117d365
Ian Parkinson <h3lloiain@gmail.com>
2026-03-27 07:08:31 -0500

    added new key binding to copy selection to system clipboard

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,10 @@ return {
       { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
       { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
       { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help Tags' },
+      { '<leader>to', '<cmd>Telescope vim_options<cr>', desc = 'List vim options'},
+      { '<leader>ts', '<cmd>Telescope spell_suggestions<cr>', desc = 'Spell suggestions'},
+      { '<leader>tm', '<cmd>Telescope lsp_document_symbols<cr>', desc = 'List symbols'},
+      { '<leader>td', '<cmd>Telescope diagnostics<cr>', desc = 'Lsp Diagnostics'}
     },
     config = function()
       local telescope = require('telescope')
@@ -27,6 +31,22 @@ return {
               ['<c-d>'] = actions.delete_buffer + actions.move_to_top,
               ['<c-k>'] = actions.move_selection_previous,
               ['<c-j>'] = actions.move_selection_next,
+              ['<c-y>'] = function(prompt_bufnr)
+                  local selection = require('telescope.actions.state').get_selected_entry()
+                  local content = selection.message or selection.text or selection.value
+
+                  if type(content) ~= "string" then
+                    content = tostring(content)
+                  end
+
+                  vim.fn.setreg('+', content)
+
+                  actions.close(prompt_bufnr)
+
+                  vim.schedule(function()
+                      vim.notify("Copied: " .. content, vim.log.levels.INFO)
+                  end)
+              end,
             },
           },
         },
