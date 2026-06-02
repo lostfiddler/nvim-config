e9d27fb12b17ea8565b87847c01b03db3d0b4293
Ian Parkinson <h3lloiain@gmail.com>
2026-06-02 08:13:03 -0500

    fix ftplugin

---
+++ ftplugin/javascript.lua
@@ -0,0 +1,5 @@
+vim.opt_local.tabstop = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
--- lua/lostfiddler/init.lua
+++ lua/lostfiddler/init.lua
@@ -1,18 +1,3 @@
 require("lostfiddler.config.options")
 require("lostfiddler.config.keymaps")
 require("lostfiddler.config.lazy")
-
-local au_ftsettings = vim.api.nvim_create_augroup("LostFiddlerFtSettings", { clear = true })
-
-vim.api.nvim_create_autocmd("FileType", {
-	group = au_ftsettings,
-	callback = function(args)
-		-- args.match contains the name of the filetype (e.g., "python")
-		local ft = args.match
-		local ok, _ = pcall(require, "lostfiddler.ftsettings." .. ft)
-		if not ok then
-			-- Optional: Quietly fail if a filetype doesn't have a config file yet
-			return
-		end
-	end,
-})
