de3b6995ca18424689a04bfd29601c47ca9fa0b6
Ian Parkinson <h3lloiain@gmail.com>
2026-06-09 21:12:00 -0500

    update config for treesitter

--- lua/lostfiddler/plugins/treesitter.lua
+++ lua/lostfiddler/plugins/treesitter.lua
@@ -1,13 +1,17 @@
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = {"typescriptreact", "javascriptreact", "html", "typescript", "javascript"},
+    callback = function() vim.treesitter.start() end,
+})
+
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup({
-        -- Ensure these are installed
-        ensure_installed = { "tsx", "typescript", "html", "javascript" },
-        highlight = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+
+			-- ts.setup() NOTE treesitter sets up itself now
+			ts.install({ "tsx", "typescript", "html", "javascript", "lua", "c", "c_sharp", "cpp", "css", "bash" })
+		end,
+	},
 }
